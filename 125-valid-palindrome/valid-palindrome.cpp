03bda61622467ec0ca0f84d0e4c476da03d2656c
Harshil Patel <harshil.patel.cg@gmail.com>
2026-05-08 12:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (7.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string res = "";
+        string res="";
 
-        for(int i = 0; i < s.size(); i++){
-            if((s[i] >= 'a' && s[i] <= 'z') || 
-               (s[i] >= 'A' && s[i] <= 'Z') ||
-                s[i]>='0' && s[i]<='9')
-            {
-                res += tolower(s[i]);
+        for(int i=0;i<s.size();i++){
+            if(
+                (s[i]>='a'&&s[i]<='z')||
+                (s[i]>='A'&& s[i]<='Z')||
+                (s[i]>='0'&&s[i]<='9')
+            ){
+                res+=tolower(s[i]);
             }
         }
+        string rev=res;
 
-        string rev = res;
-        reverse(rev.begin(), rev.end());
+        reverse(rev.begin(),rev.end());
 
-        return res == rev;
+        return rev==res;
     }
 };
