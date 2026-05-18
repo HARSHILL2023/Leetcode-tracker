2f11117727a6fe700e2a2fd2b1b8d91608a2abbd
Harshil Patel <harshil.patel.cg@gmail.com>
2026-05-18 11:27:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.76%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length();
-        int count = 0;
+        int n=s.size();
 
-        int i = n - 1;
-        while (i >= 0 && s[i] == ' ') {
+        int count=0;
+        int i = n-1;
+        while(i>=0 && s[i]==' '){
             i--;
         }
-
-        while (i >= 0 && s[i] != ' ') {
+        while(i>=0 && s[i]!=' '){
             count++;
             i--;
         }
-
         return count;
     }
 };
