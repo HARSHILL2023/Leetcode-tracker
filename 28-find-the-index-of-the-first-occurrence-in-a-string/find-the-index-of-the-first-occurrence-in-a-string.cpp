aad6b8bdacd5ab9bc60f6ce803c566de113ad37e
Harshil Patel <harshil.patel.cg@gmail.com>
2026-06-04 10:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (73.37%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int index=haystack.find(needle);return index;
+        int index=haystack.find(needle);
+        return index;
     }
 };
