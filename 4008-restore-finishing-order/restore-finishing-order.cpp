7aa37a575f5b97155efd71f26fd6bc45c098f08d
Harshil Patel <harshil.patel.cg@gmail.com>
2026-06-09 15:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (98.96%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.cpp
+++ 4008-restore-finishing-order/restore-finishing-order.cpp
@@ -3,10 +3,8 @@ public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
         vector<int> ans;
         for(int i=0;i<order.size();i++){
-            int cuurentitem=order[i];
-
             for(int j=0;j<friends.size();j++){
-                if(cuurentitem==friends[j]){
+                if(order[i]==friends[j]){
                     ans.push_back(friends[j]);
                 }
             }
