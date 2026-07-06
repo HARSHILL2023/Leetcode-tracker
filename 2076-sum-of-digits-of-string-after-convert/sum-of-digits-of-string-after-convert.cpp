c05cbf7c32ea024c63cdde90cf2557d0bd9aad0a
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-06 13:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (42.01%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string numStr = "";
+        for (char c : s) {
+            numStr += to_string(c - 'a' + 1);
+        }
+        
+        int currentSum = 0;
+        for (char c : numStr) {
+            currentSum += c - '0';
+        }
+        k--; 
+        
+        while (k > 0 && currentSum >= 10) {
+            int nextSum = 0;
+            while (currentSum > 0) {
+                nextSum += currentSum % 10;
+                currentSum /= 10;
+            }
+            currentSum = nextSum;
+            k--;
+        }
+        
+        return currentSum;
+    }
+};
