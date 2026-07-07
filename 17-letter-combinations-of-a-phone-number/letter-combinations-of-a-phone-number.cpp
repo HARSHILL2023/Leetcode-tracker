4883dd62496402a78c4c24ac6025434850a89dbb
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-07 20:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (62.11%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    
+    const vector<string> PHONE_MAP = {
+        "",     "",     "abc",  "def", 
+        "ghi",  "jkl",  "mno",  "pqrs", 
+        "tuv",  "wxyz"
+    };
+
+    void backtrack(int index, const string& digits, string& current, vector<string>& result) {
+      
+        if (index == digits.length()) {
+            result.push_back(current);
+            return;
+        }
+
+       
+        string letters = PHONE_MAP[digits[index] - '0'];
+        for (char letter : letters) {
+            current.push_back(letter);           
+            backtrack(index + 1, digits, current, result); 
+            current.pop_back();               
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if (digits.empty()) {
+            return result;
+        }
+
+        string current = "";
+        backtrack(0, digits, current, result);
+        return result;
+    }
+};
