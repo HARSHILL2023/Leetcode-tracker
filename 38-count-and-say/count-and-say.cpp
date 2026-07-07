a778377b2b8099d3ea3287515d9cc16e40622fb5
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-07 20:59:55 +0530

    Time: 4 ms (47.13%) | Memory: 10 MB (45.12%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,28 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n <= 0) return "";
+        string result = "1";
+        
+        for (int i = 1; i < n; ++i) {
+            string current = "";
+            int len = result.length();
+            
+            for (int j = 0; j < len; ++j) {
+                int count = 1;
+                while (j + 1 < len && result[j] == result[j + 1]) {
+                    count++;
+                    j++;
+                }
+                current += to_string(count) + result[j];
+            }
+            result = current;
+        }
+        
+        return result;
+    }
+};
