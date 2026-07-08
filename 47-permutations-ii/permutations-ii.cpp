a2ae1fb9c9d58f5b138fbcb0f94a99f35608c444
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-08 08:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (97.61%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void backtrack(std::vector<int>& nums, std::vector<bool>& visited, 
+                   std::vector<int>& current, std::vector<std::vector<int>>& result) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (visited[i]) continue;
+
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+
+            visited[i] = true;
+            current.push_back(nums[i]);
+
+            backtrack(nums, visited, current, result);
+
+            current.pop_back();
+            visited[i] = false;
+        }
+    }
+
+    std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        std::vector<int> current;
+        std::vector<bool> visited(nums.size(), false);
+
+        std::sort(nums.begin(), nums.end());
+
+        backtrack(nums, visited, current, result);
+        return result;
+    }
+};
