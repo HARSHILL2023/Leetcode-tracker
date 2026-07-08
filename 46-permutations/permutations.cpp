63285c62131406de49b49e181ba9172218fcc99e
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-08 08:37:18 +0530

    Time: 2 ms (36.29%) | Memory: 10.4 MB (98.37%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void backtrack(int index, std::vector<int>& nums, std::vector<std::vector<int>>& result) {
+        if (index == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for (int i = index; i < nums.size(); ++i) {
+            std::swap(nums[index], nums[i]);
+            backtrack(index + 1, nums, result);
+            std::swap(nums[index], nums[i]);
+        }
+    }
+
+    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        backtrack(0, nums, result);
+        return result;
+    }
+};
