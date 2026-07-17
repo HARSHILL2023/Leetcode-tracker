9e2d319b5af4716b569184bed1b02cb1bc9c6911
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-17 09:05:53 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/version/cpp/367-ValidPerfectSquare_20260717_090552.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/17/2026, 9:05:52 AM
+1class Solution {
+2public:
+3    bool isPerfectSquare(int num) {
+4        int left = 1;
+5        int right = num;
+6
+7        while (left <= right) {
+8            long long mid = left + (right - left) / 2;
+9            long long square = mid * mid;
+10
+11            if (square == num) {
+12                return true;
+13            }
+14            else if (square > num) {
+15                right = mid - 1;
+16            }
+17            else {
+18                left = mid + 1;
+19            }
+20        }
+21
+22        return false;
+23    }
+24};
