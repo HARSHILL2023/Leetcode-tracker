0a553aab545227a28f5dc74f9f1bc5546a7d12d9
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-21 09:07:06 +0530

    Create 34-FindFirstandLastPositionofElementinSortedArray

---
+++ 34-FindFirstandLastPositionofElementinSortedArray/version/cpp/34-FindFirstandLastPositionofElementinSortedArray_20260721_090704.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/21/2026, 9:07:04 AM
+1class Solution {
+2public:
+3    vector<int> searchRange(vector<int>& nums, int target) {
+4        int first = -1, last = -1;
+5        
+6        for (int i = 0; i < nums.size(); i++) {
+7            if (nums[i] == target) {
+8                if (first == -1) first = i; 
+9                last = i;                 
+10            }
+11        }
+12        
+13        return {first, last};
+14    }
+15};
