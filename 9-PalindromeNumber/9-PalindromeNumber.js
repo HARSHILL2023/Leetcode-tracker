6f9e5411353e175dff53887fbb8f4a98b89f53c5
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:08:38 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -0,0 +1,21 @@
+// Last updated: 7/30/2026, 10:08:37 AM
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    if (x < 0) {
+        return false;
+    }
+
+    let original = x;   
+    let reverse = 0;
+
+    while (x > 0) {
+        reverse = (reverse * 10) + (x % 10);
+        x = Math.floor(x / 10);   
+    }
+
+    return reverse === original;
+};
