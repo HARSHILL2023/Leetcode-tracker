c6fd932a0770b7c75b3856aa05126ff9901dadc6
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:59 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/30/2026, 10:07:58 AM
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+      
+        for (const string& s : strs) {
+            string sorted_s = s;
+            sort(sorted_s.begin(), sorted_s.end());
+            anagramGroups[sorted_s].push_back(s);
+        }
+        
+        
+        vector<vector<string>> result;
+        for (auto& pair : anagramGroups) {
+            result.push_back(move(pair.second));
+        }
+        
+        return result;
+    }
+};
