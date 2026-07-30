747103f0ca08b6c61cf4c34292f97dfd5e85fc97
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:05:36 +0530

    Create 3371-HarshadNumber

---
+++ 3371-HarshadNumber/3371-HarshadNumber.js
@@ -0,0 +1,16 @@
+// Last updated: 7/30/2026, 10:05:36 AM
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+    let sum=0;
+    let real=x;
+    while(real>0){
+        sum=sum+(Math.floor(real%10))
+        real=Math.floor(real/10);
+    }
+
+    if((x%sum==0)) return sum;
+    else return -1;
+};
