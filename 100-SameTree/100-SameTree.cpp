f7a7c460e1e7348e499ea4a3fe1c739fdd614fe4
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:42 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/30/2026, 10:07:42 AM
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) return true;
+        if (p == nullptr || q == nullptr) return false;
+        
+        if (p->val != q->val) return false;
+        
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
