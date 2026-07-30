bfaa71219fba0e8e559b4880257060e734acd14c
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:06:24 +0530

    Create 1482-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ 1482-HowManyNumbersAreSmallerThanTheCurrentNumber/1482-HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/30/2026, 10:06:24 AM
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        
+        unordered_map<int, int> mapping;
+        for (int i = 0; i < sorted_nums.size(); i++) {
+           
+            if (mapping.find(sorted_nums[i]) == mapping.end()) {
+                mapping[sorted_nums[i]] = i;
+            }
+        }
+        
+        vector<int> result;
+        for (int num : nums) {
+            result.push_back(mapping[num]);
+        }
+        
+        return result;
+    }
+};
