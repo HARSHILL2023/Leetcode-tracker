ff694973243afb3c4727bafe85e8f5c45e407108
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:08:03 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -0,0 +1,29 @@
+// Last updated: 7/30/2026, 10:08:02 AM
+class Solution {
+public:
+    void findCombinations(int index, int target, vector<int>& candidates, vector<int>& current, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+        
+        if (index == candidates.size()) {
+            return;
+        }
+
+        if (candidates[index] <= target) {
+            current.push_back(candidates[index]);
+            findCombinations(index, target - candidates[index], candidates, current, result);
+            current.pop_back();
+        }
+
+        findCombinations(index + 1, target, candidates, current, result);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+        findCombinations(0, target, candidates, current, result);
+        return result;
+    }
+};
