7f328c20286950e337c7eef9e145477ace14ab8b
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:55 +0530

    Create 64-MinimumPathSum

---
+++ 64-MinimumPathSum/64-MinimumPathSum.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/30/2026, 10:07:54 AM
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int minPathSum(std::vector<std::vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 1; i < m; ++i) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        for (int j = 1; j < n; ++j) {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                grid[i][j] += std::min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+};
