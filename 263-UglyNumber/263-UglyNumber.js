cb582a3088d195bab3b818ef1505564316463d1e
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:19 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.js
@@ -0,0 +1,22 @@
+// Last updated: 7/30/2026, 10:07:19 AM
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if (n <= 0) return false;
+
+    while (n % 2 === 0) {
+        n /= 2;
+    }
+
+    while (n % 3 === 0) {
+        n /= 3;
+    }
+
+    while (n % 5 === 0) {
+        n /= 5;
+    }
+
+    return n === 1;
+};
