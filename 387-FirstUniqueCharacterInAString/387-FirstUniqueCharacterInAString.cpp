34e3a65c29d6070f1fd76b61e776be46187d1eb8
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:01 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/30/2026, 10:07:00 AM
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        for(int i = 0; i < s.size(); i++) {
+            int count = 0;
+            
+            for(int j = 0; j < s.size(); j++) {
+                if(i != j && s[i] == s[j]) {
+                    count++;
+                    break; 
+                }
+            }
+            
+            if(count == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
