5a7b5673e8c4e6e13e60f3c4eea6339b46f33971
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 14:16:51 +0530

    Create 3163-StringCompressionIII

---
+++ 3163-StringCompressionIII/version/cpp/3163-StringCompressionIII_20260730_141650.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/30/2026, 2:16:50 PM
+1class Solution {
+2public:
+3    string compressedString(string word) {
+4        string res = ""; 
+5        int count = 1; 
+6        
+7        for(int i = 0; i < word.length(); i++){ 
+8            if(i + 1 < word.length() && word[i] == word[i + 1]){ 
+9                count++; 
+10            } else { 
+11                while (count > 9) {
+12                    res += to_string(9);
+13                    res += word[i];
+14                    count -= 9;
+15                }
+16                res += to_string(count); 
+17                res += word[i]; 
+18                count = 1; 
+19            } 
+20        } 
+21        
+22        return res;
+23    }
+24};
