7e95ec721a18e343378f593bf60bdefd235e2d0e
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:06:51 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ 442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/30/2026, 10:06:51 AM
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        vector<int> ans;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+
+            if(mp[nums[i]]==2){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
