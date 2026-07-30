2fc06e6aeffff8601fa2a234a18051c5898e8751
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:28 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.js
@@ -0,0 +1,22 @@
+// Last updated: 7/30/2026, 10:07:27 AM
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+
+    while(n !== 1 && n !== 4){
+
+        let sum = 0;
+
+        while(n > 0){
+            let digit = n % 10;   
+            sum = sum + digit * digit;  
+            n = Math.floor(n / 10); 
+        }
+
+        n = sum;  
+    }
+
+    return n === 1;
+};
