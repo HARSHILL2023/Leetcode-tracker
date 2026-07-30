81d18d65fabd7725640a2c396eb8fedbae5dfbc8
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:08:26 +0530

    Create 16-3sumClosest

---
+++ 16-3sumClosest/16-3sumClosest.cpp
@@ -0,0 +1,45 @@
+// Last updated: 7/30/2026, 10:08:25 AM
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+class Solution {
+public:
+    int threeSumClosest(std::vector<int>& nums, int target) {
+      
+        sort(nums.begin(), nums.end());
+        
+        int closestSum = nums[0] + nums[1] + nums[2];
+        int n = nums.size();
+
+       
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+               
+                if (currentSum == target) {
+                    return currentSum;
+                }
+
+              
+                if (std::abs(currentSum - target) < std::abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+              
+                if (currentSum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+};
