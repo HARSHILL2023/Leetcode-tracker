cfbba0aa39a8d2ea35104ddc39c0f8e958edd5ac
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:08:41 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,19 @@
+// Last updated: 7/30/2026, 10:08:40 AM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        let diff = target - nums[i];
+
+        if(map.has(diff)){
+            return [map.get(diff), i];
+        }
+
+        map.set(nums[i], i);
+    }
+}; 
