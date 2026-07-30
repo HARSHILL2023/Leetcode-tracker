8be7bb47d4c216835e89af34b9395cde923e4408
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:05:31 +0530

    Create 3931-ProcessStringWithSpecialOperationsI

---
+++ 3931-ProcessStringWithSpecialOperationsI/3931-ProcessStringWithSpecialOperationsI.cpp
@@ -0,0 +1,26 @@
+// Last updated: 7/30/2026, 10:05:30 AM
+class Solution {
+public:
+    string processStr(string s) {
+        string res ;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                if (!res.empty()) {
+                    res.pop_back();
+                }
+            }
+            else if(s[i]=='#'){
+                res+=res;
+            } 
+            else if(s[i]=='%'){
+                    reverse(res.begin(),res.end());
+            }
+            else {
+                res += s[i];
+            }
+        }
+
+        return res;
+    }
+};
