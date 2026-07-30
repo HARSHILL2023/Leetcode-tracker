17e1c80e127415c615077f671cd928c293a47edb
Harshil Patel <harshil.patel.cg@gmail.com>
2026-07-30 10:07:25 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/30/2026, 10:07:24 AM
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+           
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size()&&abs(i - j) <= k;j++){
+                if(nums[i] == nums[j]) return true;
+               
+            }
+        }
+
+        return false;
+
+        
+    }
+};
