4bcc0e0baababdb55a60f2fea227c098b2643292
Harshil Patel <harshil.patel.cg@gmail.com>
2026-08-07 14:35:23 +0530

    Create 82-RemoveDuplicatesfromSortedListII

---
+++ 82-RemoveDuplicatesfromSortedListII/version/cpp/82-RemoveDuplicatesfromSortedListII_20260807_143521.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/7/2026, 2:35:21 PM
+1class Solution {
+2public:
+3    ListNode* deleteDuplicates(ListNode* head) {
+4        ListNode* dummy = new ListNode(0, head);
+5        ListNode* prev = dummy;
+6        
+7        while (head != nullptr) {
+8            if (head->next != nullptr && head->val == head->next->val) {
+9                while (head->next != nullptr && head->val == head->next->val) {
+10                    head = head->next;
+11                }
+12                prev->next = head->next;
+13            } else {
+14                prev = prev->next;
+15            }
+16            head = head->next;
+17        }
+18        
+19        ListNode* result = dummy->next;
+20        delete dummy;
+21        return result;
+22    }
+23};
