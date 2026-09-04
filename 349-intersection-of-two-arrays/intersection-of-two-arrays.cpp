eee789a24355cc925a3a1feac109b61c896f2774
Harshil Patel <harshil.patel.cg@gmail.com>
2026-09-04 09:49:27 +0530

    Time: 4 ms (22.48%) | Memory: 14.1 MB (66.80%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+          unordered_set<int> s1(nums1.begin(), nums1.end());
+            vector<int> ans;
+          for(auto i:nums2){
+            if(s1.count(i)==1){
+                ans.push_back(i);
+                s1.erase(i);
+            }
+          }
+          return ans;
+    }
+};
