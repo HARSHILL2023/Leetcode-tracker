3b73894fc32d48d3f111f6c4e011de370f2326dd
Harshil Patel <harshil.patel.cg@gmail.com>
2026-09-06 10:51:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (30.49%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int scoreOfString(string s) {
         int score = 0;
         for (int i = 0; i < s.length() - 1; ++i) {
-            score += std::abs(s[i] - s[i + 1]);
+            score +=abs(s[i] - s[i + 1]);
         }
         return score;
     }
