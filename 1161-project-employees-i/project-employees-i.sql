abf09b029bce2ca8f3f30ad9db37aa6d1d58ede5
Harshil Patel <harshil.patel.cg@gmail.com>
2026-09-16 10:28:49 +0530

    Time: 1029 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select P.project_id ,ROUND(AVG(e.experience_years), 2) as average_years from Project P inner join Employee E on P.employee_id=E.employee_id group by project_id;
